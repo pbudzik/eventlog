696aa48799d531526db0417deb35e0a6b5dc4e4c
Przemek <pbudzik@gmail.com>
2020-01-04 16:57:12 +0100

    mix.exs details added

--- mix.exs
+++ mix.exs
@@ -11,6 +11,22 @@ defmodule MessageLog.MixProject do
     ]
   end
 
+  defp description do
+    """
+    Append log with indexing.
+    """
+  end
+
+  defp package do
+    [
+     files: ["lib", "mix.exs", "README.md"],
+     maintainers: ["PB"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/pbudzik/eventlog",
+              "Docs" => "http://hexdocs.pm/eventlog/"}
+     ]
+  end
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
